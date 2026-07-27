6254755252d54b45206ccc93b3f9bf5afc841086
HoangNM_NTA <hoangnm.nta@gmail.com>
2026-07-27 16:56:06 +0700

    update v2.4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require_relative "../../app/models/active_storage_service_config"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
